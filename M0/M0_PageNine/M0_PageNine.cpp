db10fe1f17f4653e0973a2fe089518adb51e80d8
joshzash14 <joshzash@hotmail.com>
2024-08-24 20:47:29 -0400

    Intro project code to be used as base guide as needed

---
+++ M0/M0_PageNine/M0_PageNine.cpp
@@ -0,0 +1,22 @@
+/*
+* Student Name: Joshua Betancourt
+* File Name: ###.cpp
+* Date: ##/##/2024
+*/
+
+#include <iostream>
+
+using namespace std;
+#include <conio.h>
+
+int main()
+{
+    // All programs are required to have two blank lines at the start of the program and margins must have two blank spaces from the left of the screen.
+    cout << "\n\n Hello World!" << endl;
+    printf("\n Welcome to my first not so useful C++ Program");
+    cout << "\n\n Press any key to continue ...";
+    _getch();
+
+    return 0;
+}
+
