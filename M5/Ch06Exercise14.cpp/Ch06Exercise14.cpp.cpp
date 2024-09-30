1d58443c017917c0cc40c0ed70c6222d7088bd73
joshzash14 <joshzash@hotmail.com>
2024-09-30 01:53:07 -0400

    updated code to add input function for low income as I needed the low income as a boolan for proper fee calculation. Had bug due to having lowIncome as a char. Setting it to string itself did not solve it as invalid inputs could break program. In future all inputs should be sanitized in a similar manner

--- M5/Ch06Exercise14.cpp/Ch06Exercise14.cpp.cpp
+++ M5/Ch06Exercise14.cpp/Ch06Exercise14.cpp.cpp
@@ -1,74 +1,55 @@
-
 /*
     Student Name: Joshua Betancourt
     File Name: Ch06Exercise14.cpp.cpp
     Date: 09/30/2024
-    Description: This program is used to
-
-    During the tax season, every Friday, the J&J accounting firm provides assistance to people who prepare their own tax returns. 
-    Their charges are as follows:
-    If a person has low income (<= 25,000) and the consulting time is less than or equal to 30 minutes, there are no charges; 
-    otherwise, the service charges are 40% of the regular hourly rate for the time over 30 minutes.
-    
-    For others, if the consulting time is less than or equal to 20 minutes, there are no service charges; 
-    otherwise, service charges are 70% of the regular hourly rate for the time over 20 minutes.
-    
-    (For example, suppose that a person has low income and spent 1 hour and 15 minutes, 
-    and the hourly rate is $70.00. Then the billing amount is 70.00 x 40 x (45/60) = $21.00.)
-    Write a program that prompts the user to enter the hourly rate, the total consulting time, 
-    and whether the person has low income. 
-    
-    The program should output the billing amount. 
-    
-    Your program must contain a function that takes as input the hourly rate, 
-    the total consulting time, 
-    and a value indicating whether the person has low income. 
-    The function should return the billing amount. 
-    
-    Your program may prompt the user to enter the consulting time in minutes.
+    Description: This program is used to calculate a customer's service consulting charge based on the provided information.
+        Inputs: If customer is low income
+                How many minutes of consulting where provided
+                The consultant's hourly rate
 */
 
-#include <conio.h>
-#include <iostream>
-using namespace std;
+#include <conio.h>              // needed for getch()
+#include <iostream>             // needed for cout, cin
+#include <string>               // needed for string input/input validation
+#include <iomanip>              // needed for fixed, setprecision(), locale
 
-
-float calculateBillingAmount (float hourlyRate, int consultingTime, char lowIncome);
+float calculateBillingAmount(float hourlyRate, int consultingTime, bool lowIncome);
+bool lowIncomeInputToBoolean();
 
 int main()
 {
+    // Setting the formatting for all number outputs
+    std::cout << std::fixed << std::setprecision(2); // for setting the decimal precision
+    std::locale loc(""); // for the thousands separator 
+    std::cout.imbue(loc);
+
     // creating local variables for program
     float hourlyRate;
     int consultingTime;
-    char lowIncome;
     float consultingFee;
+    bool lowIncome = false;
 
     // Welcome user to program and then  begin prompting them for needed information
-    cout << "\n\n  Welcome, let us calculate the cost to file this customer's return!" << endl;
+    std::cout << "\n\n  Welcome, let us calculate the cost to file this customer's return!" << std::endl;
 
-    cout << "\n  Please indicate if the customer is a low income earner: "
-         << "\n    (Y) If earned income for this year is at or under $25,000"
-         << "\n    (N) If earned income for this year is at or above $25,001"
-         << "\n  Answer: ";
-    cin >> lowIncome;
-    cout << endl; 
+    // Have user identify if customer is low income earner
+    lowIncome = lowIncomeInputToBoolean();
 
-    cout << "\n  Please provide the consulting time provided today in whole minutes: ";
-    cin >> consultingTime;
-    cout << endl;
+    std::cout << "\n  Please provide the consulting time provided today in whole minutes: ";
+    std::cin >> consultingTime;
+    std::cout << std::endl;
 
-    cout << "\n  Please provide your hourly rate: ";
-    cin >> hourlyRate;
-    cout << endl;
+    std::cout << "\n  Please provide your hourly rate: ";
+    std::cin >> hourlyRate;
+    std::cout << std::endl;
 
     consultingFee = calculateBillingAmount(hourlyRate, consultingTime, lowIncome);
 
-    cout << "\n  The charge for today's service is: $" << consultingFee << ".";
-
+    std::cout << "\n  The charge for today's service is: $" << consultingFee << ".";
 
     // Display THE closing messageS for non Visual Studio IDEs
-    cout << "\n  Thanks for using my program!" << endl;
-    cout << "\n\n  Press any key to continue ..." << endl;
+    std::cout << "\n  Thanks for using my program!" << std::endl;
+    std::cout << "\n\n  Press any key to continue ..." << std::endl;
 
     _getch();  // halt processing 
 
@@ -76,17 +57,53 @@ int main()
 }
 
 
-float calculateBillingAmount (float hourlyRate, int consultingTime, char lowIncome) {
+float calculateBillingAmount (float hourlyRate, int consultingTime, bool lowIncome) {
+    // setting fee to zero as there are only two scenario's in which the fee is not zero
     float consultingFee = 0.00;
 
+    // calculating consultingFee if service falls within one of two boundaries
     if (lowIncome && (consultingTime > 30)) {
-        cout << "in cacle";
-        int chargedTime = consultingTime - 30;
+        float chargedTime = consultingTime - 30;
         consultingFee = (hourlyRate * 0.40) * (chargedTime / 60);
     } else if (!lowIncome && consultingTime > 20) {
-        int chargedTime = consultingTime - 20;
+        float chargedTime = consultingTime - 20;
         consultingFee = (hourlyRate * 0.70) * (chargedTime / 60); 
     }
 
     return consultingFee;
 };
+
+// prompt user to identify if customer is a low income earner and convert input to boolean
+bool lowIncomeInputToBoolean () {
+    bool lowIncomeBoolean = false;
+    std::string updatedInput;
+    bool validInput = false;
+
+    std::cout << "\n  Please indicate if the customer is a low income earner: "
+         << "\n    (Y) If earned income for this year is at or under $25,000"
+         << "\n    (N) If earned income for this year is at or above $25,001"
+         << "\n  Answer: ";
+    std::cin >> updatedInput;
+    std::cout << std::endl; 
+
+
+    while (!validInput) {
+    if (updatedInput == "y" || updatedInput == "Y") {
+        validInput = true;
+        lowIncomeBoolean = true;
+        break;
+    } else if (updatedInput == "n" || updatedInput == "N") {
+        validInput = true;
+        break;
+    } else
+        std::cout << "\n The provided input was not valid. Please try again."
+                  << "\n\n  Please indicate if the customer is a low income earner: "
+                  << "\n    (Y) If earned income for this year is at or under $25,000"
+                  << "\n    (N) If earned income for this year is at or above $25,001"
+                  << "\n  Answer: ";
+        std::cin >> updatedInput;
+        std::cout << std::endl; 
+    } 
+
+    return lowIncomeBoolean;
+}
