40cfa428ad13c4fec62779ea918371f5ba96bfe2
joshzash14 <joshzash@hotmail.com>
2024-10-07 00:54:50 -0400

    updating functions and removing code not relevent to this exercise

--- M6/Ch07Exercise4/Ch07Exercise4.cpp
+++ M6/Ch07Exercise4/Ch07Exercise4.cpp
@@ -3,10 +3,9 @@
     Student Name: Joshua Betancourt
     File Name: Ch07Exercise4.cpp
     Date: 10/07/2024
-    Description: This program is used to calculate a customer's service consulting charge based on the provided information.
-        Inputs: If customer is low income
-                How many minutes of consulting where provided
-                The consultant's hourly rate
+    Description: This program is used to remove all vowels from a provided string.
+        Input: any string by user
+
  
 Write a program that prompts the user to input a string. 
 The program then uses the function substr to remove all the vowels from the string. 
@@ -19,37 +18,24 @@ Your program must contain a function to remove all the vowels and a function to 
 #include <iostream>             // needed for cout, cin
 #include <string>               // needed for string input/input validation
 
-float calculateBillingAmount(float hourlyRate, int consultingTime, bool lowIncome);
+std::string removeVowels(std::string providedString);
 std::string captureStringInput();
 
 int main()
 {
 
     // creating local variables for program
-    float hourlyRate;
-    int consultingTime;
-    float consultingFee;
-    bool lowIncome = false;
-
     std::string userInput;
+    std::string updatedString;
 
     // Welcome user to program and then  begin prompting them for needed information
     std::cout << "\n\n  Welcome, let me trim you string of any vowels!" << std::endl;
 
     // Have user identify if customer is low income earner
     userInput = captureStringInput();
+    updatedString = removeVowels(userInput);
 
-    std::cout << "\n  Please provide the consulting time provided today in whole minutes: ";
-    std::cin >> consultingTime;
-    std::cout << std::endl;
-
-    std::cout << "\n  Please provide your hourly rate: ";
-    std::cin >> hourlyRate;
-    std::cout << std::endl;
-
-    consultingFee = calculateBillingAmount(hourlyRate, consultingTime, lowIncome);
-
-    std::cout << "\n  The charge for today's service is: $" << consultingFee << ".";
+    std::cout << "\n  Your updated string without any vowels is: " << updatedString << ".";
 
     // Display the closing messages for non Visual Studio IDEs
     std::cout << "\n  Thanks for using my program!" << std::endl;
@@ -60,23 +46,14 @@ int main()
     return 0;  // exit code
 }
 
-float calculateBillingAmount (float hourlyRate, int consultingTime, bool lowIncome) {
-    // setting fee to zero as there are only two scenario's in which the fee is not zero
-    float consultingFee = 0.00;
+// Scrapes vowels from provided string
+std::string removeVowels(std::string providedString) {
+    std::string formattedString;
 
-    // calculating consultingFee if service falls within one of two boundaries
-    if (lowIncome && (consultingTime > 30)) {
-        float chargedTime = consultingTime - 30;
-        consultingFee = (hourlyRate * 0.40) * (chargedTime / 60);
-    } else if (!lowIncome && consultingTime > 20) {
-        float chargedTime = consultingTime - 20;
-        consultingFee = (hourlyRate * 0.70) * (chargedTime / 60); 
-    }
-
-    return consultingFee;
+    return formattedString;
 };
 
-// prompt user to identify if customer is a low income earner and convert input to boolean
+// prompt user to provide a string. Returns string without spaces, numbers or special characters
 std::string captureStringInput() {
     std::string updatedInput;
     bool validInput = false;
@@ -104,5 +81,5 @@ std::string captureStringInput() {
         std::cout << std::endl; 
     } 
 
-    return validInput;
+    return updatedInput;
 }
