78fc9e3f2dbc7a4eb4eefee0c3ae06e923848917
joshzash14 <joshzash@hotmail.com>
2024-11-01 00:53:45 -0400

    adding discussion project

---
+++ M10/M10_Discussion_JoshB.cpp/M10_Discussion_JoshB.cpp.cpp
@@ -0,0 +1,23 @@
+// M10_Discussion_JoshB.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+    Student Name: Joshua Betancourt
+    File Name: M10_Discussion_JoshB.cpp
+    Date: 10/31/2024
+    Description: This program is using pointers and contains errors needing to be debugged.
+*/
+
+#include <conio.h>              // needed for getch()
+#include <iostream>             // needed for cout, cin
+#include <string>               // needed for string
+
+struct car 
+{
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
