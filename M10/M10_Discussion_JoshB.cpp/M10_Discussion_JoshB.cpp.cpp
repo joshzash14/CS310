9841dd06ea4f8f14fc43ecbfe17a4faa182da6b9
joshzash14 <joshzash@hotmail.com>
2024-11-01 01:30:21 -0400

    Progam now in working state

--- M10/M10_Discussion_JoshB.cpp/M10_Discussion_JoshB.cpp.cpp
+++ M10/M10_Discussion_JoshB.cpp/M10_Discussion_JoshB.cpp.cpp
@@ -10,14 +10,45 @@
 #include <iostream>             // needed for cout, cin
 #include <string>               // needed for string
 
-struct car 
+struct carType 
 {
-
+    std::string make;
+    std::string model;
+    std::string color;
+    int year;
+    int mileage;
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // introduction to the program and what is does
+    std::cout << "\n\n  Welcome to my program."
+              << "\n  I'll be outputting information about a car struct I've made using a pointer. \n";
+
+    carType* fancyCar = new carType;
+
+    (*fancyCar).make = "uber fance";
+    (*fancyCar).model = "fancy pants";
+    (*fancyCar).color = "orange";
+    (*fancyCar).year = 2026;
+    (*fancyCar).mileage = 26;
+
+
+    std::cout << "\n  Let's look at my car!"
+              << "\n  I have here a " << (*fancyCar).year
+              << " " << (*fancyCar).make << " " << (*fancyCar).model
+              << "\n  This puppy is brand new and only has " <<  (*fancyCar).mileage 
+              << " miles!";
+
+    delete fancyCar;
+
+    // Display the closing messages for non Visual Studio IDEs
+    std::cout << "\n\n  Thanks for using my program!" << std::endl;
+    std::cout << "\n\n  Press any key to continue ..." << std::endl;
+
+    _getch();  // halt processing 
+
+    return 0;
 }
 
 
