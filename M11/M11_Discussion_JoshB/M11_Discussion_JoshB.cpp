14cce2a6ff16c55acf4b21fde7a4f6731630c8e1
joshzash14 <joshzash@hotmail.com>
2024-11-08 01:32:25 -0500

    Code in working state

--- M11/M11_Discussion_JoshB/M11_Discussion_JoshB.cpp
+++ M11/M11_Discussion_JoshB/M11_Discussion_JoshB.cpp
@@ -16,19 +16,22 @@
 template <class elemType>
 class Items
 {
-    public:
-        void addItem(std::string providedItem);
-        void printItems();
-        Items(int arrayLength = 5);
-        ~Items()
     private:
         int maxLength;
         int length;
-        Items *itemArray;
+        elemType *itemArray;
+    public:
+        void addItem(elemType providedItem);
+        void printItems();
+        ~Items();
+        Items(int arrayLength = 5) : maxLength(arrayLength), length(0)
+        {
+            itemArray = new elemType[maxLength];
+        }
 };
 template <class elemType>
-void Items<elemType>::addItem(std::string providedItem)
-{
+void Items<elemType>::addItem(elemType providedItem)
+{   
     itemArray[length] = providedItem;
     length++;
 }
@@ -37,20 +40,14 @@ void Items<elemType>::printItems()
 {
     for (int i = 0; i < length; i++)
     {
-        std::cout << itemArray[i] << " "
+        std::cout << itemArray[i] << " ";   
     }
     
 }
 template <class elemType>
-Items<elemType>::Items(int arrayLength)
-{
-    maxLength = arrayLength;
-    length = 0;
-    itemArray = new elemType[maxLength];
-}
-template <class elemType>
 Items<elemType>::~Items()
 {
+    delete[] itemArray;
 }
 
 
@@ -62,8 +59,23 @@ int main()
               << "\n  Once created I will output the items in my array"
               << std::endl;
 
+    Items <std::string> colors;
+    Items <int> numbers(3);
 
+    colors.addItem("blue");
+    colors.addItem("yellow");
+    colors.addItem("silver");
+    colors.addItem("orange");
+    colors.addItem("pINk");
 
+    numbers.addItem(4);
+    numbers.addItem(15);
+
+    std::cout << "\n  The colors I have stored are: ";
+    colors.printItems();
+
+    std::cout << "\n  The numbers I have stored are: ";
+    numbers.printItems();
 
 
     // Display the closing messages for non Visual Studio IDEs
